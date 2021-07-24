5a2b93e9357b1d57b3abc6ab9a685509d83308c3
Alejandro Herrera <alef.herrera@gmail.com>
2021-07-24 03:55:36 -0300

    add tests to dynamo implementation

--- internal/user/infrastructure/dynamo_repository.go
+++ internal/user/infrastructure/dynamo_repository.go
@@ -62,24 +62,62 @@ func (receiver *dynamoRepository) GetOne(ctx context.Context, id string) (*model
 		return nil, nil
 	}
 
+	return itemToUser(result.Item)
+}
+
+func (receiver *dynamoRepository) Update(ctx context.Context, model *models.User) error {
+	return receiver.Store(ctx, model)
+}
+
+func (receiver *dynamoRepository) GetAll(ctx context.Context) ([]models.User, error) {
+	result, err := receiver.dynamo.ScanWithContext(ctx, &dynamodb.ScanInput{
+		TableName: aws.String(receiver.table),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	output := make([]models.User, len(result.Items))
+
+	for i, item := range result.Items {
+
+		user, err := itemToUser(item)
+
+		if err != nil {
+			return nil, err
+		}
+
+		output[i] = *user
+	}
+
+	return output, nil
+}
+
+func (receiver *dynamoRepository) Delete(ctx context.Context, id string) error {
+	_, err := receiver.dynamo.DeleteItemWithContext(ctx, &dynamodb.DeleteItemInput{
+		TableName: aws.String(receiver.table),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(id),
+			},
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func itemToUser(input map[string]*dynamodb.AttributeValue) (*models.User, error) {
 	var item models.User
-	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+	err := dynamodbattribute.UnmarshalMap(input, &item)
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &item, nil
-}
-
-func (receiver *dynamoRepository) Update(ctx context.Context, model *models.User) error {
-	panic("implement me")
-}
-
-func (receiver *dynamoRepository) GetAll(ctx context.Context) ([]models.User, error) {
-	panic("implement me")
-}
-
-func (receiver *dynamoRepository) Delete(ctx context.Context, id string) error {
-	panic("implement me")
 }
--- internal/user/infrastructure/dynamo_repository_test.go
+++ internal/user/infrastructure/dynamo_repository_test.go
@@ -27,4 +27,90 @@ func Test_dynamoRepository_Store(t *testing.T) {
 	assert.NoError(t, err)
 	assert.NotNil(t, getResult)
 	assert.Equal(t, user.Id, getResult.Id)
+	err = repository.Delete(ctx, userId.String())
+	assert.NoError(t, err)
+}
+
+func Test_dynamoRepository_Update(t *testing.T) {
+	dynamo, err := util.InitDynamo()
+	assert.NoError(t, err)
+	repository := NewDynamoRepository(dynamo, "Users")
+	ctx := context.TODO()
+	userId := uuid.New()
+	user := models.User{
+		Id:       userId.String(),
+		Username: "Name",
+	}
+	err = repository.Store(ctx, &user)
+	assert.NoError(t, err)
+	modifiedUser := models.User{
+		Id:       user.Id,
+		Username: "NewName",
+	}
+	err = repository.Update(ctx, &modifiedUser)
+	assert.NoError(t, err)
+	assert.Equal(t, "NewName", modifiedUser.Username)
+	getResult, err := repository.GetOne(ctx, user.Id)
+	assert.NoError(t, err)
+	assert.Equal(t, modifiedUser.Username, getResult.Username)
+	assert.NotEqual(t, user.Username, getResult.Username)
+	err = repository.Delete(ctx, user.Id)
+	assert.NoError(t, err)
+}
+
+func Test_dynamoRepository_GetAll(t *testing.T) {
+	dynamo, err := util.InitDynamo()
+	assert.NoError(t, err)
+	repository := NewDynamoRepository(dynamo, "Users")
+	ctx := context.TODO()
+
+	for i := 0; i < 15; i++ {
+		user := models.User{
+			Id: uuid.New().String(),
+		}
+		err = repository.Store(ctx, &user)
+		assert.NoError(t, err)
+	}
+
+	all, err := repository.GetAll(ctx)
+	assert.NoError(t, err)
+	assert.Len(t, all, 15)
+
+	for _, user := range all {
+		err := repository.Delete(ctx, user.Id)
+		assert.NoError(t, err)
+	}
+}
+
+func Test_dynamoRepository_Delete(t *testing.T) {
+	dynamo, err := util.InitDynamo()
+	assert.NoError(t, err)
+	repository := NewDynamoRepository(dynamo, "Users")
+	ctx := context.TODO()
+
+	user1 := models.User{
+		Id: uuid.New().String(),
+	}
+	err = repository.Store(ctx, &user1)
+	assert.NoError(t, err)
+
+	user2 := models.User{
+		Id: uuid.New().String(),
+	}
+	err = repository.Store(ctx, &user2)
+	assert.NoError(t, err)
+
+	all, err := repository.GetAll(ctx)
+	assert.NoError(t, err)
+	assert.Len(t, all, 2)
+
+	err = repository.Delete(ctx, user2.Id)
+	assert.NoError(t, err)
+
+	result, err := repository.GetAll(ctx)
+	assert.NoError(t, err)
+	assert.Len(t, result, 1)
+
+	err = repository.Delete(ctx, user1.Id)
+	assert.NoError(t, err)
 }
